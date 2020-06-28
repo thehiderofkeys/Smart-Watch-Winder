e8fb733e06952ca6e72622b9d282d5ae42877901
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-06-28 18:47:48 +1200

    Add mongoose connection to server set up

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import session from 'express-session';
 import {createServer} from 'http';
 import socketIO from 'socket.io';
+import mongoose from 'mongoose';
 
 import apiRouter from './api';
 
 const PORT = process.env.PORT || 10000;
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/SmartWatchWinder';
 
 const app = express();
 app.use(
@@ -16,6 +18,11 @@ app.use(
     }),
 );
 app.use('/api', apiRouter);
+
+mongoose.connect(DB_URI, {useNewUrlParser: true}).then(
+    () => console.log('Connected to database!'),
+    (err) => console.error(err),
+);
 
 const server = createServer(app);
 const sockets = socketIO(server);
