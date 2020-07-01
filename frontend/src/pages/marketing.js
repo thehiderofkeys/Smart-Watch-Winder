a11d1bb7423e75c18f4c3d55741cda38725f0ea9
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-07-01 21:47:37 +1200

    Update desktop marketing page information

--- frontend/src/pages/marketing.js
+++ frontend/src/pages/marketing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Container, Grid, makeStyles} from '@material-ui/core';
+import {Container, Grid, Typography, Button, makeStyles} from '@material-ui/core';
 
 import {ReactComponent as WordMark} from '../images/WordMark.svg';
 import WinderAndMockup from '../images/WinderAndMockup.png';
@@ -21,6 +21,21 @@ function MarketingPage() {
         <Grid item xs={9}>
           <img src={WinderAndMockup} className={classes.mockup}/>
         </Grid>
+        <Grid item xs={3}>
+          <Typography variant='h4' align='right' className={classes.contrastingText}>
+            A watch winder that&apos;s smart.
+          </Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <Typography variant='h5'> Not a winder for your smart watch. </Typography>
+          <Typography variant='h6'>
+            This winder is connected to the internet. So you can control your winder, wherever you are.
+          </Typography>
+        </Grid>
+        <Grid item xs={3}/>
+        <Grid item xs={9}>
+          <Button variant="contained" color="secondary">Take it for a spin!</Button>
+        </Grid>
       </Grid>
     </Container>
   );
@@ -39,14 +54,18 @@ const useStyles = makeStyles((theme) => ({
     'top': '0',
     'left': '0',
     'bottom': '0',
-    'width': 'calc(50vw - 260px)',
-    'minWidth': 'calc(28.5vw + 16px)',
+    'width': 'calc(50vw - 310px)',
+    'minWidth': 'calc(24.5vw + 16px)',
     'background': '#246ce9',
     'zIndex': '-1',
   },
   mockup: {
+    marginLeft: '-5%',
     maxWidth: '90%',
     maxHeight: '100%',
+  },
+  contrastingText: {
+    color: 'white',
   },
 }));
 
