f5d282cfac4d73dd80a17daf3bd55e3c141280de
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-07-27 01:30:59 +1200

    Add redux store to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}><App /></Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
